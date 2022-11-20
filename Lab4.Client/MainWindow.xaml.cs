cc98bca814be3de735f6ec63acc7d27b4e85d406
MaksymLehkyi <92023843+MaksymLehkyi@users.noreply.github.com>
2022-11-20 22:26:25 +0200

    client changes

--- Lab4.Client/MainWindow.xaml.cs
+++ Lab4.Client/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace Lab4.Client
 {
@@ -20,37 +21,102 @@ namespace Lab4.Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        HubConnection connection;
         public MainWindow()
         {
             InitializeComponent();
         }
+        private async Task HubConnection_Closed(Exception? arg)
+        {
+            await Task.Delay(new Random().Next(0, 5) * 1000);
+            await connection.StartAsync();
+        }
 
         private void ChatStart_Click(object sender, RoutedEventArgs e)
         {
-            ChatBlock.Text = "";
+
+
+            try
+            {
+                connection.InvokeAsync("StartChat");
+                ChatBlock.Text += "\nChat was Started";
+
+            }
+            catch (Exception ex)
+            {
+                ChatBlock.Text += ex.Message;
+            }
         }
 
-        private void Connect_Click(object sender, RoutedEventArgs e)
+        private async void Connect_Click(object sender, RoutedEventArgs e)
         {
-            /*test кнопки удалить*/
-            string tempLabel = new string("");
-            tempLabel = NicknameField.Text;
-            NicknameLabel.Content=tempLabel;
+            connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:31182/chat")
+                .Build();
+            //connection.Closed += HubConnection_Closed; // сделать проверку был ли введен юзер
+            
+            string name = new string("");
+            name = NicknameField.Text;
+
+            try
+            {
+                await connection.StartAsync();
+                await connection.InvokeAsync("Connect",name);
+                ChatBlock.Text += "\n"+ name+" - User connected";
+                //ListBox1.Items.Add(name+ "User connected");
+                NicknameLabel.Content = name;
+                connection.On<string, ulong, ulong>("UserConnected", (user, _p, _g) =>
+                {
+                    var newMessage = $"{name}:{_p}:{_g}";
+                    //ChatBlock.Text = "\n" + Message;
+                    ListBox1.Items.Add(newMessage);
+                });
+                
+            }
+            catch (Exception ex)
+            {
+                ChatBlock.Text += "\n"+ex.Message;
+            }
+
+
+            /*connection.On<string, ulong, ulong>("UserConnected", (user, _p, _g) =>
+                {
+                    var newMessage = $"{name}:{11}:{7}";
+                });*/
+
         }
 
         private void Send_Click(object sender, RoutedEventArgs e)
         {
-            /*test кнопки удалить*/
+            string message = new string("");
+            message = TextInput.Text;
+            try
+            {
+                connection.InvokeAsync("SendMessage", message);
+                connection.On<string>("ReceiveMessage", (message) =>
+                {
+                    //ChatBlock.Text += message;
+                    ListBox1.Items.Add(message);
+                });
+            }
+            catch (Exception ex)
+            {
+                ChatBlock.Text += ex.Message;
+            }
+
+
+
+            /*test кнопки удалить
             string notconn = "Not connected";
             if (NicknameLabel.Content.ToString() != notconn && Content.ToString() != notconn )
             {
-                string tempLabel = new string("");
-                tempLabel = NicknameField.Text;
+                
                 string temtext = new string("");
                 temtext = TextInput.Text;
-                ChatBlock.Text += "\n>>" + tempLabel + " Says: " + temtext;
+                ChatBlock.Text += "\n>>" + tempLabel1 + " Says: " + temtext;
             }
             else ChatBlock.Text += "\n" + notconn;
+            */
         }
     }
 }
